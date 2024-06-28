1cee641bfc9691b99645a44c7ac5069bc0d20645
kennedysferreira <sferreira.kennedy@gmail.com>
2024-06-28 15:56:06 +0100

    finish project

--- srcjs/router.js
+++ srcjs/router.js
@@ -9,15 +9,13 @@ export class Router {
     this.routes[routeName] = page;
   }
 
-  // Método para tratar os eventos de navegação
   route(event) {
     event.preventDefault();
-    const pathname = event.target.pathname;
+    const { pathname } = event.target;
     window.history.pushState({}, "", pathname);
     this.handle(pathname);
   }
 
-  // Método para renderizar o conteúdo da rota atual
   handle(pathname) {
     const route = this.routes[pathname] || this.routes["/404"];
 
@@ -25,14 +23,25 @@ export class Router {
       .then((data) => data.text())
       .then((html) => {
         element.app.innerHTML = html;
+        this.setActiveLink(pathname);
       });
   }
 
-  // Inicializa os links na página
+  setActiveLink(pathname) {
+    element.links.forEach((link) => {
+      if (link.getAttribute("href") === pathname) {
+        link.classList.add("active");
+      } else {
+        link.classList.remove("active");
+      }
+    });
+  }
+
   initializeLinks() {
     element.links.forEach((link) => {
       link.addEventListener("click", this.route.bind(this));
     });
     this.handle(window.location.pathname);
+    this.setActiveLink(window.location.pathname);
   }
 }
